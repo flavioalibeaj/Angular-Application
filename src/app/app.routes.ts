0cb4bc11fbfd915bdc225b21f2afa1a1f4038d63
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-07-03 14:27:33 +0200

    changed directories

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,20 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.routes').then((r) => r.routes),
+    loadChildren: () =>
+      import('./pages/auth/auth.routes').then((r) => r.routes),
     // TODO guard with can activate and can activate child
   },
   {
     // TODO guard with can activate and can activate child
     path: '',
-    loadChildren: () => import('./home/home.routes').then((r) => r.routes),
+    loadChildren: () =>
+      import('./pages/home/home.routes').then((r) => r.routes),
   },
   {
     path: 'not-found',
     loadComponent: () =>
-      import('./not-found/not-found.component').then(
+      import('./pages/not-found/not-found.component').then(
         (c) => c.NotFoundComponent
       ),
   },
