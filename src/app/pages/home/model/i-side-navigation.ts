fab7055d0c17ab2c7d5293f271fc279991b98908
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-07-04 00:06:49 +0200

    fixed layout

---
+++ src/app/pages/home/model/i-side-navigation.ts
@@ -0,0 +1,4 @@
+export interface ISideNavigation {
+  path: string;
+  title: string;
+}
--- src/app/pages/home/pages/layout/layout.component.ts
+++ src/app/pages/home/pages/layout/layout.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ISideNavigation } from '../../model/i-side-navigation';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    RouterOutlet,
+    MatListModule,
+    RouterLink,
+  ],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
-
+  readonly sideNavigation: ISideNavigation[] = [
+    { path: '', title: 'Home' },
+    { path: 'not-found', title: 'Not Found' },
+    { path: 'auth', title: 'Auth' },
+  ];
 }
