30b643109b7a41bcf8d2a4c97d85008e4140a4ca
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-07-04 10:14:28 +0200

    changes

--- src/app/pages/home/pages/layout/layout.component.ts
+++ src/app/pages/home/pages/layout/layout.component.ts
@@ -6,6 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ISideNavigation } from '../../model/i-side-navigation';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-layout',
@@ -18,11 +20,15 @@ import { ISideNavigation } from '../../model/i-side-navigation';
     RouterOutlet,
     MatListModule,
     RouterLink,
+    MatBadgeModule,
+    MatMenuModule,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
+  notifications = []; // TODO websocket notifications
+
   readonly sideNavigation: ISideNavigation[] = [
     { path: '', title: 'Home' },
     { path: 'not-found', title: 'Not Found' },
