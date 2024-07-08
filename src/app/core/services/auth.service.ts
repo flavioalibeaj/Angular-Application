1ae35745b09711ac55cab212c17b85c9ca12ba51
Flavio Alibeaj <flavio@dev.al>
2024-07-08 15:01:34 +0200

    login feature

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -7,22 +7,32 @@ import {
   signal,
 } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpService } from './http.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly _router: Router = inject(Router);
-  public token: WritableSignal<string> = signal<string>(
+  private _token: WritableSignal<string> = signal<string>(
     localStorage.getItem('token') ?? ''
   );
+  private readonly _router: Router = inject(Router);
+  private readonly _httpService: HttpService = inject(HttpService);
+  public set token(newToken: string) {
+    this._token.set(newToken);
+    localStorage.setItem('token', newToken);
+  }
 
   public readonly isAuthenticated: Signal<boolean> = computed(() => {
-    console.log('tok', this.token());
-    return !!this.token();
+    return !!this._token();
   }); //TODO decrypt token and check exp date
 
-  logout() {
+  public login<T>(body: T): Observable<{ token: string }> {
+    return this._httpService.post<T, { token: string }>('auth/login', body);
+  }
+
+  public logout() {
     localStorage.clear();
     this._router.navigate(['auth']);
   }
--- src/app/pages/auth/pages/login/login.component.ts
+++ src/app/pages/auth/pages/login/login.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { AuthService } from '../../../../core/services/auth.service';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatDividerModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  public hide = true;
+  public readonly formGroup: FormGroup = new FormGroup({
+    username: new FormControl<string>('mor_2314', Validators.required),
+    password: new FormControl<string>('83r5^_', Validators.required),
+  });
+  private readonly _authService: AuthService = inject(AuthService);
+  private readonly _router: Router = inject(Router);
 
+  onSubmit() {
+    this._authService
+      .login(this.formGroup.getRawValue())
+      .pipe(
+        tap((response) => {
+          if (response.token) {
+            this._authService.token = response.token;
+            this._router.navigate(['']);
+          }
+        })
+      )
+      .subscribe();
+  }
 }
